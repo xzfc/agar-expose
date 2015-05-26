878bcf7fc23e073bde7825ee1fe5867e36bf63e5
Albert Safin <xzfcpw@gmail.com>
2015-05-26 12:01:09 +0600

    <Commit message was censored out>

--- expose.user.js
+++ expose.user.js
@@ -1,36 +1,44 @@
 // ==UserScript==
-// @name         Agar.io Expose
-// @match        http://agar.io/
-// @run-at       document-start
-// @version      1.0
-// @grant        none
+// @name        Agar.io Expose
+// @version     1.1
+// @namespace   xzfc
+// @updateURL   https://raw.githubusercontent.com/xzfc/agar-expose/master/expose.user.js
+// @include     http://agar.io/
+// @run-at      document-start
+// @grant       none
 // ==/UserScript==
+
+if (window.top != window.self)
+    return;
 
 var observer = new MutationObserver(function(mutations) {
     for (var i = 0; i < mutations.length; i++) {
         if (/^http:\/\/agar\.io\/main_out\.js/.test(mutations[i].addedNodes[0].src)) {
             document.head.removeChild(mutations[i].addedNodes[0]);
             observer.disconnect();
+            runRequest();
             break;
         }
     }
 });
 observer.observe(document.head, {childList: true});
 
-var request = new XMLHttpRequest();
-request.onload = function() {
-    handleResponse(this.responseText);
-};
-request.onerror = function() {
-    console.log("Response was null");
-};
-request.open("get", "http://agar.io/main_out.js", true);
-request.send();
+function runRequest() {
+    var request = new XMLHttpRequest();
+    request.onload = function() {
+        handleResponse(this.responseText);
+    };
+    request.onerror = function() {
+        console.log("Response was null");
+    };
+    request.open("get", "http://agar.io/main_out.js", true);
+    request.send();
+}
 
 function handleResponse(response) {
     var cutStrings = response.match(/(\w+)\.push\(this\);/);
     var split = response.split(cutStrings[0]);
-    response = split[0] + cutStrings[0] + "window.agar.allCells=" + cutStrings[1] + ";window.agar.myCells=B;window.agar.top=y;" + split[1];
+    response = "window.agar={};" + split[0] + cutStrings[0] + "window.agar.allCells=" + cutStrings[1] + ";window.agar.myCells=B;window.agar.top=y;window.agar.top_teams=u;window.agar.ws = m.url;" + split[1];
     var script = document.createElement("script");
     script.innerHTML = response;
     insertScript(script);
@@ -40,6 +48,5 @@ function insertScript(script) {
     if (typeof jQuery === "undefined") {
         return setTimeout(insertScript, 0, script);
     }
-    window.agar = {}
     document.head.appendChild(script);
 }
