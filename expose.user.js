17b4cfabe0aacc5ac77eaf29023c42298fd9b8b2
Albert Safin <xzfcpw@gmail.com>
2015-05-27 14:40:37 +0600

    Make Expose more robust

--- expose.user.js
+++ expose.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Agar.io Expose
-// @version     1.1
+// @version     1.2
 // @namespace   xzfc
 // @updateURL   https://raw.githubusercontent.com/xzfc/agar-expose/master/expose.user.js
 // @include     http://agar.io/
@@ -9,44 +9,57 @@
 // ==/UserScript==
 
 if (window.top != window.self)
-    return;
+    return
 
 var observer = new MutationObserver(function(mutations) {
     for (var i = 0; i < mutations.length; i++) {
         if (/^http:\/\/agar\.io\/main_out\.js/.test(mutations[i].addedNodes[0].src)) {
-            document.head.removeChild(mutations[i].addedNodes[0]);
-            observer.disconnect();
-            runRequest();
-            break;
+            document.head.removeChild(mutations[i].addedNodes[0])
+            observer.disconnect()
+            runRequest()
+            break
         }
     }
-});
-observer.observe(document.head, {childList: true});
+})
+observer.observe(document.head, {childList: true})
 
 function runRequest() {
-    var request = new XMLHttpRequest();
+    var request = new XMLHttpRequest()
     request.onload = function() {
-        handleResponse(this.responseText);
-    };
-    request.onerror = function() {
-        console.log("Response was null");
-    };
-    request.open("get", "http://agar.io/main_out.js", true);
-    request.send();
-}
-
-function handleResponse(response) {
-    var cutStrings = response.match(/(\w+)\.push\(this\);/);
-    var split = response.split(cutStrings[0]);
-    response = "window.agar={};" + split[0] + cutStrings[0] + "window.agar.allCells=" + cutStrings[1] + ";window.agar.myCells=B;window.agar.top=y;window.agar.top_teams=u;window.agar.ws = m.url;" + split[1];
-    var script = document.createElement("script");
-    script.innerHTML = response;
-    insertScript(script);
+        var script = document.createElement("script")
+        script.innerHTML = modify(this.responseText)
+        insertScript(script)
+        console.log("Expose: replacement done")
+    }
+    request.onerror = function() { console.error("Expose: response was null") }
+    request.open("get", "http://agar.io/main_out.js", true)
+    request.send()
 }
 
 function insertScript(script) {
-    if (typeof jQuery === "undefined") {
-        return setTimeout(insertScript, 0, script);
+    if (typeof jQuery === "undefined")
+        return setTimeout(insertScript, 0, script)
+    document.head.appendChild(script)
+}
+
+function modify(text) {
+    var reset = ""
+    function replace(what, from, to, defaultValue) {
+        var newText = text.replace(from, to)
+        if(newText === text) {
+            console.error("Expose: " + what + " replacement failed!")
+        } else {
+            text = newText
+            if(defaultValue !== undefined)
+                reset += "window.agar." + what + "=" + defaultValue + ";"
+        }
     }
-    document.head.appendChild(script);
+    replace("allCells", /(\w+)\.push\(this\);/,     "$&" + "window.agar.allCells=$1;",         '[]')
+    replace("myCells",  /(case 32:)(\w+)(\.push)/,  "$1" + "window.agar.myCells=$2;" + "$2$3", '[]')
+    replace("top",      /case 49:[^:]+?(\w+)=\[];/, "$&" + "window.agar.top=$1;",              '[]')
+    replace("ws",       /new WebSocket\((\w+)\);/,  "$&" + "window.agar.ws=$1;",               '""')
+    replace("topTeams", /case 50:(\w+)=\[];/,       "$&" + "window.agar.topTeams=$1;",         '[]')
+    replace("reset",    /new WebSocket\(\w+\);/,    "$&" + reset)
+
+    return "window.agar={};" + reset + text
 }
