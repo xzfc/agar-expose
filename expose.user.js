3e7340230ff594923cea3c8b1edbda6c0af8005c
Albert Safin <xzfcpw@gmail.com>
2015-06-08 22:12:54 +0600

    Make Expose more robust.
    Before starting observer, check if document.head already contains main_out.js.

--- expose.user.js
+++ expose.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Agar.io Expose
-// @version     1.4
+// @version     1.5
 // @namespace   xzfc
 // @updateURL   https://raw.githubusercontent.com/xzfc/agar-expose/master/expose.user.js
 // @include     http://agar.io/*
@@ -11,23 +11,35 @@
 if (window.top != window.self)
     return
 
-var observer = new MutationObserver(function(mutations) {
-    for (var i = 0; i < mutations.length; i++) {
-        var addedNodes = mutations[i].addedNodes
-        for (var j = 0; j < addedNodes.length; j++) {
-            var addedNode = addedNodes[j]
-            if (/^http:\/\/agar\.io\/main_out\.js/.test(addedNode.src)) {
-                document.head.removeChild(addedNode)
-                observer.disconnect()
-                runRequest()
-                return
-            }
+function runExpose(){
+    if (document.readyState !== 'loading')
+        return console.error("Expose: this script should run at document-start")
+
+    for(var i = 0; i < document.head.childNodes.length; i++)
+        if(tryReplace(document.head.childNodes[i]))
+            return
+
+    function observerFunc(mutations) {
+        for (var i = 0; i < mutations.length; i++) {
+            var addedNodes = mutations[i].addedNodes
+            for (var j = 0; j < addedNodes.length; j++)
+                if(tryReplace(addedNodes[j])) {
+                    observer.disconnect()
+                    return
+                }
         }
     }
-})
-observer.observe(document.head, {childList: true})
+    var observer = new MutationObserver(observerFunc)
+    observer.observe(document.head, {childList: true})
+}
 
-function runRequest() {
+runExpose()
+
+function tryReplace(node) {
+    if (!/^http:\/\/agar\.io\/main_out\.js/.test(node.src))
+        return false
+    document.head.removeChild(node)
+    
     var request = new XMLHttpRequest()
     request.onload = function() {
         var script = document.createElement("script")
@@ -38,6 +50,8 @@ function runRequest() {
     request.onerror = function() { console.error("Expose: response was null") }
     request.open("get", "http://agar.io/main_out.js", true)
     request.send()
+    
+    return true
 }
 
 function insertScript(script) {
