xzfc/agar-expose

commit 6fb6de6a993bb804841f7c35ac30d0a3cff23862

Fix inline script handling in Firefox

expose.user.js
@@ -63,6 +63,7 @@ if (window.top != window.self)
 if (document.readyState !== 'loading')
     return console.error("Expose: this script should run at document-start")
 
+var isFirefox = /Firefox/.test(navigator.userAgent)
 
 // Stage 1: Find corresponding rule
 var rules
@@ -75,29 +76,39 @@ if (!rules)
     return console.error("Expose: cant find corresponding rule")
 
 
-// Stage 2: Iterate over document.head child elements and look for `main_out.js`
-for (var i = 0; i < document.head.childNodes.length; i++)
-    if (tryReplace(document.head.childNodes[i]))
-        return
-// If there are no desired element in document.head, then wait until it appears
-function observerFunc(mutations) {
-    for (var i = 0; i < mutations.length; i++) {
-        var addedNodes = mutations[i].addedNodes
-        for (var j = 0; j < addedNodes.length; j++)
-            if (tryReplace(addedNodes[j]))
-                return observer.disconnect()
+// Stage 2: Search for `main_out.js`
+if (isFirefox) {
+    function bse_listener(e) { tryReplace(e.target, e) }
+    window.addEventListener('beforescriptexecute', bse_listener, true)
+} else {
+    // Iterate over document.head child elements and look for `main_out.js`
+    for (var i = 0; i < document.head.childNodes.length; i++)
+        if (tryReplace(document.head.childNodes[i]))
+            return
+    // If there are no desired element in document.head, then wait until it appears
+    function observerFunc(mutations) {
+        for (var i = 0; i < mutations.length; i++) {
+            var addedNodes = mutations[i].addedNodes
+            for (var j = 0; j < addedNodes.length; j++)
+                if (tryReplace(addedNodes[j]))
+                    return observer.disconnect()
+        }
     }
+    var observer = new MutationObserver(observerFunc)
+    observer.observe(document.head, {childList: true})
 }
-var observer = new MutationObserver(observerFunc)
-observer.observe(document.head, {childList: true})
-
 
 // Stage 3: Replace found element using rules
-function tryReplace(node) {
+function tryReplace(node, event) {
     var scriptLinked = rules.scriptUriRe && rules.scriptUriRe.test(node.src)
     var scriptEmbedded = rules.scriptTextRe && rules.scriptTextRe.test(node.textContent)
     if (node.tagName != "SCRIPT" || (!scriptLinked && !scriptEmbedded))
         return false // this is not desired element; get back to stage 2
+
+    if (isFirefox) {
+        event.preventDefault()
+        window.removeEventListener('beforescriptexecute', bse_listener, true)
+    }
 
     var mod = {
         reset: "",
@@ -120,7 +131,10 @@ function tryReplace(node) {
     if (scriptEmbedded) {
         mod.text = node.textContent
         rules.replace(mod)
-        node.textContent = mod.get()
+        document.head.removeChild(node)
+        var script = document.createElement("script")
+        script.textContent = mod.get()
+        document.head.appendChild(script)
         console.log("Expose: replacement done")
     } else {
         document.head.removeChild(node)

commit 3984a6e6f55ec15ce0b1674052fc903793c66287

Fix inline script handling in Chrome
Bug: two simultaneously running `main_out.js` scripts

This bug was introduced in 6fb6de6a993bb804841f7c35ac30d0a3cff23862

expose.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Agar.io Expose
-// @version     2.4
+// @version     2.5
 // @namespace   xzfc
 // @updateURL   https://raw.githubusercontent.com/xzfc/agar-expose/master/expose.user.js
 // @include     http://agar.io/*
@@ -133,10 +133,14 @@ function tryReplace(node, event) {
     if (scriptEmbedded) {
         mod.text = node.textContent
         rules.replace(mod)
-        document.head.removeChild(node)
-        var script = document.createElement("script")
-        script.textContent = mod.get()
-        document.head.appendChild(script)
+        if (isFirefox) {
+            document.head.removeChild(node)
+            var script = document.createElement("script")
+            script.textContent = mod.get()
+            document.head.appendChild(script)
+        } else {
+            node.textContent = mod.get()
+        }
         console.log("Expose: replacement done")
     } else {
         document.head.removeChild(node)